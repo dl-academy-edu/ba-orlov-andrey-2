bcce997fb0530f1a7f2e6be525c30eedf5ba129a
sem6703 <sem6703@yandex.ru>
2021-05-11 19:08:06 +0300

    svg иконка и др.

--- src/pages/toys/index.js
+++
@@ -1,6 +0,0 @@
-var range = document.querySelector('.range');
-
-range.addEventListener('input', function () {
-	var i = (this.value*100)/this.getAttribute('max');
-	this.style.background = '-webkit-linear-gradient(left, #C29974 0%, #C29974 '+i+'%, rgba(0,0,0,0.5) '+i+'%, rgba(0,0,0,0.5) 100%)';
-})
